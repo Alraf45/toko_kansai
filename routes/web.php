7e48975b5fb2d01a21fd22b6fdbb0830d39ea10c
h3ct3c <hect3ch@gmail.com>
2025-07-22 08:06:28 +0700

    update navbar

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\AuthController;
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
