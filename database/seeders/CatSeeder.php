a88b820ec335712575b54add7c44ea840f9b61db
Alraf45 <143483629+Alraf45@users.noreply.github.com>
2025-07-24 15:55:16 +0700

    UPDATE CRUD

--- database/seeders/CatSeeder.php
+++ database/seeders/CatSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cat;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class CatSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i = 0; $i < 12; $i++) {
+            Cat::create([
+                'No' => $faker->increments,
+                'Gambar' => $faker->imagePath($storagePath = 'storage/img',$width = 640, $height = 480, 'cats', false),
+                'Nama Produk' => $faker->name(['Kansai Anti Mosquito','Kansai FTALIT DUO','Kansai FTALIT','Kansai Glimmer','Kansai Pearl Sheen','Kansai Property Eksterior','Kanssai Property','Kansai Tropic','Kansai Diamond-Shield','RAIN BLOCK Kaleng Plastik','SPLESH Kaleng PLastik']),
+                'Kategori' => $faker->randomElement(['Cat Eksterior', 'Cat Interior', 'Cat Besi&Kayu', 'Cat Premium']),
+                'Warna' => $faker->randomElement(['Neutral', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple']),
+                'Stok' => $faker->numberBetween(1, 100),
+                'Harga' => $faker->numberBetween(10000, 100000),
+            ]);
+        }
     }
 }
