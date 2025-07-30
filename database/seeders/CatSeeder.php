98140d387bf1e0277f38f46d614129fbceaf5fef
Alraf45 <143483629+Alraf45@users.noreply.github.com>
2025-07-30 08:08:03 +0700

    UPDATE DB

--- database/seeders/CatSeeder.php
+++ database/seeders/CatSeeder.php
@@ -14,15 +14,14 @@ class CatSeeder extends Seeder
     public function run(): void
     {
         $faker = \Faker\Factory::create('id_ID');
-        for ($i = 0; $i < 12; $i++) {
+        for ($i = 0; $i < 11; $i++) {
             Cat::create([
-                'No' => $faker->increments,
-                'Gambar' => $faker->imagePath($storagePath = 'storage/img',$width = 640, $height = 480, 'cats', false),
-                'Nama Produk' => $faker->name(['Kansai Anti Mosquito','Kansai FTALIT DUO','Kansai FTALIT','Kansai Glimmer','Kansai Pearl Sheen','Kansai Property Eksterior','Kanssai Property','Kansai Tropic','Kansai Diamond-Shield','RAIN BLOCK Kaleng Plastik','SPLESH Kaleng PLastik']),
+                'Gambar' => 'storage/img/' . $faker->randomElement(['Kansai Anti Mosquito.png', 'KANSAI FTALIT DUO.png', 'Kansai FTALIT.png', 'Kansai Glimmer.png', 'Kansai Pearl Sheen.png', 'Kansai Property Eksterior.png', 'Kanssai Property.png', 'Kansai Tropic.png', 'Kansai Diamond-Shield.png', 'RAIN BLOCK Kaleng Plastik.png', 'SPLESH Kaleng PLastik.png']),
+                'Nama Produk' => $faker->randomElement(['Kansai Anti Mosquito','Kansai FTALIT DUO','Kansai FTALIT','Kansai Glimmer','Kansai Pearl Sheen','Kansai Property Eksterior','Kansai Property','Kansai Tropic','Kansai Diamond-Shield','RAIN BLOCK Kaleng Plastik','SPLESH Kaleng PLastik']),
                 'Kategori' => $faker->randomElement(['Cat Eksterior', 'Cat Interior', 'Cat Besi&Kayu', 'Cat Premium']),
                 'Warna' => $faker->randomElement(['Neutral', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple']),
-                'Stok' => $faker->numberBetween(1, 100),
-                'Harga' => $faker->numberBetween(10000, 100000),
+                'Stok' => $faker->numberBetween(300,1000),
+                'Harga' => $faker->numberBetween(100000, 1000000), // Adjusted to a more realistic range
             ]);
         }
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CatSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(CatSeeder::class);
+
+        User::factory(10)->create([
+        ]);
+
+
 
         User::factory()->create([
             'name' => 'Test User',
@@ -22,3 +28,4 @@ class DatabaseSeeder extends Seeder
         ]);
     }
 }
+
